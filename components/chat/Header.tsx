6f9c08d2d1943e987987b11b5179f35125f56f9a
James Hagood <cardfaux@tutanota.com>
2023-04-28 19:36:23 -0500

    moved some chat code into components

---
+++ components/chat/Header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+export default function Header({
+  stack,
+  params,
+}: {
+  stack: { logo: string; info: string };
+  params: { stack: string };
+}) {
+  return (
+    <div className='header flex bg-slate-200 p-4 rounded-2xl'>
+      <div className='flex mr-4 justify-center items-center'>
+        <Image src={stack.logo} width={200} height={200} alt={`${params.stack} stack`} />
+      </div>
+      <div className='flex font-bold text-sm'>{stack.info}</div>
+    </div>
+  );
+}
