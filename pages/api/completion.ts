6ab999feb64f3fdc31379859724cd85f18f182ed
James Hagood <cardfaux@tutanota.com>
2023-04-29 15:26:51 -0500

    got chatgpt to work

--- pages/api/completion.ts
+++ pages/api/completion.ts
@@ -1,15 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_CHATGPT_API_KEY,
+});
 
 export default async function completion(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const body = req.body;
     const prompt = body.prompt || '';
 
-    const aiResponse = 'React JS is a library for creating UIs...';
+    try {
+      const openai = new OpenAIApi(configuration);
+      const completion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt,
+        temperature: 0.7,
+        max_tokens: 1024,
+      });
 
-    await new Promise((res) => setTimeout(res, 500));
-
-    return res.status(200).json({ result: aiResponse });
+      const aiResponse = completion?.data?.choices[0]?.text?.trim();
+      return res.status(200).json({ result: aiResponse });
+    } catch (error) {
+      let message;
+      if (error instanceof Error) message = error.message;
+      else message = String(error);
+      reportError({ message, name: 'completion', body });
+      console.log(message);
+      return res.status(500).json({ error: { message: message } });
+    }
   } else {
     return res.status(500).json({ error: { message: 'Invalid Api Route' } });
   }
