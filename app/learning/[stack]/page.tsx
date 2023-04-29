50fd74b66d726132be8199aeb64863858d8b82da
James Hagood <cardfaux@tutanota.com>
2023-04-28 23:51:53 -0500

    still fucked

--- app/learning/[stack]/page.tsx
+++ app/learning/[stack]/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/chat/Header';
 import { Message } from '@/components/chat/Message';
 import Prompt from '@/components/chat/Prompt';
+import { usePromptStore } from '@/zustand/store';
 
 async function getStacks() {
   const res = await fetch(`${process.env.BASE_URL}/api/get-stacks`);
@@ -20,9 +21,8 @@ export default async function Stack({ params }: { params: { stack: string } }) {
       <hr className='my-4' />
 
       <div className='chat flex flex-col h-full overflow-scroll'>
-        {/* <Message author='human' idx={0} avatar='https://thrangra.sirv.com/Avatar2.png' text='What is React JS?' />
-        <Message author='human' idx={1} avatar='/logo-open-ai.png' text='React JS is a library for creating UIs...' /> */}
-        <Message />
+        <Message author='human' idx={0} avatar='https://thrangra.sirv.com/Avatar2.png' text='What is React JS?' />
+        <Message author='human' idx={1} avatar='/logo-open-ai.png' text='React JS is a library for creating UIs...' />
       </div>
       <div className='flex p-4'>
         <Prompt />
