7f42d84cf7c7e8aabc456383a0df89ad34b0d0b1
James Hagood <cardfaux@tutanota.com>
2023-04-30 17:40:38 -0500

    just fortcing rebuild again

--- components/chat/Header.tsx
+++ components/chat/Header.tsx
@@ -13,7 +13,7 @@ export default function Header({
   return (
     <div className='header flex bg-slate-200 p-4 rounded-2xl'>
       <div className='flex mr-4 justify-center items-center'>
-        <Image src={stack.logo} width={200} height={200} alt={`${params.stack} stack`} />
+        <Image src={stack.logo || '/apple-logo.png'} width={200} height={200} alt={`${params.stack} stack`} />
       </div>
       <div className='flex font-bold text-sm'>{stack.info}</div>
     </div>
